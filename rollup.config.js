e88e9f23e3dceb4bd9dcab86d8ef2e7ae1935c5f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-11-30 21:29:40 +0100

    chore: Setup project

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from 'rollup-plugin-babel'
+
+// rollup.config.js
+export default {
+  input: `src/index.js`,
+  output: [{
+    file: `dist/index.js`,
+    format: 'cjs'
+  }, {
+    file: `dist/index.es.js`,
+    format: 'es'
+  }],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      externalHelpers: false,
+      runtimeHelpers: true
+    })
+  ],
+}
