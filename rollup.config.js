8fe1c3ad6ac76c89cedee59ca04cc74de372da35
Kennedy Kori <kennedykori47@gmail.com>
2021-01-12 04:26:41 +0300

    chore: setup project for typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
-import babel from 'rollup-plugin-babel'
+import typescript from "@rollup/plugin-typescript"
 
 // rollup.config.js
 export default {
-  input: `src/index.js`,
-  output: [{
-    file: `dist/index.js`,
-    format: 'cjs'
-  }, {
-    file: `dist/index.es.js`,
-    format: 'es'
-  }],
+  input: `src/index.ts`,
+  output: [
+    {
+      file: `dist/index.js`,
+      format: "cjs",
+    },
+    {
+      file: `dist/index.es.js`,
+      format: "es",
+    },
+  ],
   external: ["path-to-regexp"],
   plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      externalHelpers: false,
-      runtimeHelpers: true
-    })
+    typescript(),
   ],
 }
